f02878aaa59dae1bca7ce4b1e2e5293ca3d383e7
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-08 13:47:40 +0000

    Loading card displays for async operations

---
+++ src/components/LoadingList.tsx
@@ -0,0 +1,23 @@
+import { EuiFlexGrid } from '@elastic/eui';
+import React, { ReactNode } from 'react';
+
+import LoadingCard from './LoadingCard';
+
+const LoadingList = () => {
+  const cardList = new Array<any>(1, 2, 3, 4, 5, 6, 7, 8);
+  cardList.fill(<LoadingCard />);
+
+  console.log(cardList);
+
+  return (
+    <>
+      <EuiFlexGrid columns={4} style={{ marginTop: '3rem' }}>
+        {cardList.map((card) => {
+          return card;
+        })}
+      </EuiFlexGrid>
+    </>
+  );
+};
+
+export default LoadingList;
