e53dc63304711be0651a1813e901e909e60eef16
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-08 14:09:16 +0000

    UserMenu prototyping

---
+++ src/components/Navbar/UserMenu.tsx
@@ -0,0 +1,98 @@
+import {
+    EuiAvatar,
+    EuiFlexGroup,
+    EuiFlexItem,
+    EuiHeaderSectionItemButton,
+    EuiLink,
+    EuiPopover,
+    EuiSpacer,
+    EuiText,
+    useGeneratedHtmlId,
+} from '@elastic/eui';
+import { useState } from 'react';
+
+import { useAuth } from '../../stores/AuthContext';
+
+const UserMenu = () => {
+  const { user } = useAuth();
+
+  const headerUserPopoverId = useGeneratedHtmlId({
+    prefix: 'headerUserPopover',
+  });
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onMenuButtonClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  const button = (
+    <EuiHeaderSectionItemButton
+      aria-controls={headerUserPopoverId}
+      aria-expanded={isOpen}
+      aria-haspopup="true"
+      aria-label="Account menu"
+      onClick={onMenuButtonClick}
+    >
+      <EuiAvatar
+        name={user?.name ?? ''}
+        size="s"
+        imageUrl={user?.avatar_url ?? ''}
+      />
+    </EuiHeaderSectionItemButton>
+  );
+
+  return (
+    <EuiPopover
+      id={headerUserPopoverId}
+      button={button}
+      isOpen={isOpen}
+      anchorPosition="downRight"
+      closePopover={closeMenu}
+      panelPaddingSize="none"
+    >
+      <div style={{ width: 320 }}>
+        <EuiFlexGroup
+          gutterSize="m"
+          className="euiHeaderProfile"
+          responsive={false}
+        >
+          <EuiFlexItem grow={false}>
+            <EuiAvatar
+              name={user?.name ?? ''}
+              size="xl"
+              imageUrl={user?.avatar_url ?? ''}
+            />
+          </EuiFlexItem>
+
+          <EuiFlexItem>
+            <EuiText>
+              <p>{user?.name}</p>
+            </EuiText>
+
+            <EuiSpacer size="m" />
+
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                <EuiFlexGroup justifyContent="spaceBetween">
+                  <EuiFlexItem grow={false}>
+                    <EuiLink>Edit profile</EuiLink>
+                  </EuiFlexItem>
+
+                  <EuiFlexItem grow={false}>
+                    <EuiLink>Log out</EuiLink>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </div>
+    </EuiPopover>
+  );
+};
+
+export default UserMenu;
