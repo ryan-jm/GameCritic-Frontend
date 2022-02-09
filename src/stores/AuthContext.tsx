6f03fa68f90b73ef453500736e0437d9991be3e5
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-09 15:13:55 +0000

    refactor to use api modules

--- src/stores/AuthContext.tsx
+++ src/stores/AuthContext.tsx
@@ -1,28 +1,12 @@
-import axios, { AxiosInstance } from 'axios';
-import { createContext, FC, useContext, useEffect, useState } from 'react';
+import { createContext, FC, useContext, useState } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
-interface IUser {
-  username: string;
-  name: string;
-  avatar_url: string;
-}
-
-interface IAuthContext {
-  isAuthed: boolean;
-  user?: IUser | null;
-  token?: string;
-  login: (username: string, password: string) => Promise<AxiosInstance>;
-  logout: (username: string) => Promise<void> | void;
-}
-
-const defaultClient = axios.create({
-  baseURL: 'https://gamecritic.herokuapp.com/api/',
-});
+import * as Auth from '../api/Auth';
+import { IAuthContext, IUser } from '../types/auth.types';
 
 const defaultState = {
   isAuthed: false,
-  login: async () => defaultClient,
+  login: async () => {},
   logout: async () => {},
 };
 
@@ -30,45 +14,23 @@ const AuthContext = createContext<IAuthContext>(defaultState);
 
 export const AuthProvider: FC = ({ children }) => {
   const [isAuthed, setAuthStatus] = useState(defaultState.isAuthed);
-  const [token, setToken] = useState('');
   const [user, setUser] = useState<IUser | null>(null);
 
   const login = async (username: string, password: string) => {
-    const {
-      data: { token },
-    } = await axios.post('https://gamecritic.herokuapp.com/api/auth', {
-      username,
-      password,
-    });
-    if (token) {
-      const newClient = axios.create({
-        baseURL: 'https://gamecritic.herokuapp.com/api/',
-        headers: { token: token },
-      });
-      const {
-        data: { user },
-      } = await newClient.get(`users/${username}`);
-      if (user) {
-        setUser(user);
-        setToken(token);
-        setAuthStatus(true);
-        return newClient;
-      }
+    const user = await Auth.validate(username, password);
+    if (user) {
+      setUser(user);
+      setAuthStatus(true);
     }
-
-    return axios.create({
-      baseURL: 'https://gamecritic.herokuapp.com/api/',
-    });
   };
 
   const logout = (username: string) => {
     setAuthStatus(false);
-    setToken('');
     setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthed, token, user, login, logout }}>
+    <AuthContext.Provider value={{ isAuthed, user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
