04daf7fb8d6ea0a832fe252283126de966e48785
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-10 09:35:35 +0000

    add basename for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import '@elastic/eui/dist/eui_theme_dark.css';
-import '@elastic/eui/dist/eui_theme_light.css';
+import "@elastic/eui/dist/eui_theme_dark.css";
+import "@elastic/eui/dist/eui_theme_light.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/gamecritic-frontend/">
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
