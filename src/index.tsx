1eae36edd3fc7c55c520ffa24de9a0bd5700272e
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-10 10:18:02 +0000

    change baseurl

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import "@elastic/eui/dist/eui_theme_dark.css";
-import "@elastic/eui/dist/eui_theme_light.css";
+import '@elastic/eui/dist/eui_theme_dark.css';
+import '@elastic/eui/dist/eui_theme_light.css';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter basename="/gamecritic-frontend/">
+  <BrowserRouter basename="/GameCritic-Frontend/">
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
