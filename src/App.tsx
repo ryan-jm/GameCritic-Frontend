1c05e1e8e61a5ebfede8ad57d7a4078415720a5f
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-11 11:31:43 +0000

    category route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
+import Categories from './pages/Categories';
 import Dashboard from './pages/Dashboard';
 import Error from './pages/Error';
 import Home from './pages/Home';
@@ -43,6 +44,7 @@ function App() {
               />
             </Route>
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/categories" element={<Categories />} />
           </Routes>
         </AuthProvider>
       </EuiProvider>
