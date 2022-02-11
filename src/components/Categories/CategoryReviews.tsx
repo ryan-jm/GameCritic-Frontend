bcb04f458c440f13c40b9d9052b90060c9e0395e
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-11 11:32:16 +0000

    display categories with modals

---
+++ src/components/Categories/CategoryReviews.tsx
@@ -0,0 +1,36 @@
+import { EuiCard, EuiFlexGroup, EuiFlexItem, EuiIcon, EuiLink } from '@elastic/eui';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Review } from '../../types/review.types';
+
+interface ICategoryReviewsProps {
+  reviews: Array<Review> | Array<any>;
+}
+
+const CategoryReviews = ({ reviews }: ICategoryReviewsProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <EuiFlexGroup gutterSize="l" direction="column">
+      {reviews.map((review: Review | null, index: number) => (
+        <EuiFlexItem>
+          <EuiCard
+            display="subdued"
+            layout="horizontal"
+            key={review?.review_id || `review-${index}`}
+            title={review?.title ?? ''}
+            icon={<EuiIcon type={review?.review_img_url ?? ''} size="xxl" />}
+            onClick={() => navigate(`/reviews/${review?.review_id}`)}
+          >
+            <EuiLink>
+              <span>See review...</span>
+            </EuiLink>
+          </EuiCard>
+        </EuiFlexItem>
+      ))}
+    </EuiFlexGroup>
+  );
+};
+
+export default CategoryReviews;
